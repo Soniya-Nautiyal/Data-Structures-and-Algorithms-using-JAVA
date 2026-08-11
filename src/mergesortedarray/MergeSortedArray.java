4fe4d6619818af2885980a22efeacda02137c6c0
s488 <soniya@dxc.com>
2026-08-12 00:48:21 +0530

    merged sorted array

--- src/mergesortedarray/MergeSortedArray.java
+++ src/mergesortedarray/MergeSortedArray.java
@@ -1,51 +1,48 @@
-package com.dxc.msgcommon.utility.common;
+package mergesortedarray;
 
 import java.util.Arrays;
 
-public class MergeSortedArray
-{
-	public static void main(String[] args)
-	{
-        int[] array1= new int[]{0,3,4,31};
-        int[] array2= new int[]{4,6,30};
-        MergeSortedArray utl = new MergeSortedArray();
-        System.out.println(Arrays.toString(utl.mergeSortedArrays(array1, array2)));
-    }
+public class MergeSortedArray {
+	public static void main(String[] args) {
+		int[] array1 = new int[] { 0, 3, 4, 31 };
+		int[] array2 = new int[] { 4, 6, 30 };
+		MergeSortedArray utl = new MergeSortedArray();
+		System.out.println(Arrays.toString(utl.mergeSortedArrays(array1, array2)));
+	}
 
-    private int[] mergeSortedArrays(int[] array1, int[] array2) {
-        int totallength = array1.length+array2.length;
-        if(array1==null || array2==null)
-            return null;
+	private int[] mergeSortedArrays(int[] array1, int[] array2) {
+		
+		if (array1 == null || array2 == null)
+			return null;
+		
+		int totallength = array1.length + array2.length;
 
-        if(array1.length ==1 && array2.length == 1){
-            if(array1[0] > array2[0]){
-                return new int[]{array2[0], array1[0]};
-            } else {
-                return new int[]{array1[0], array2[0]};
-            }
-        }
+		int[] mergedArray = new int[totallength];
 
-        if(array1.length == 0 && array2.length > 0)
-            return array2;
-        if(array1.length > 0 && array2.length == 0)
-            return array1;
+		int firstArrayPointer = 0;
+		int secondArrayPointer = 0;
+		int i = 0;
+		while (i < totallength) {
 
-        int[] mergedArray = new int[totallength];
+			if (secondArrayPointer >= array2.length) {
+				mergedArray[i] = array1[firstArrayPointer];
+				firstArrayPointer++;
+			} else if (firstArrayPointer >= array1.length) {
+				mergedArray[i] = array2[secondArrayPointer];
+				secondArrayPointer++;
 
-        int firstArrayPointer = 0;
-        int secondArrayPointer = 0;
-        int i=0;
-        while(i< totallength){
-            if(array1[firstArrayPointer] < array2[secondArrayPointer]){
-                mergedArray[i] = array1[firstArrayPointer];
-                firstArrayPointer++;
-            } else {
-                mergedArray[i] = array2[secondArrayPointer];
-                secondArrayPointer++;
-            }
-            i++;
-        }
+			} else {
+				if (array1[firstArrayPointer] < array2[secondArrayPointer]) {
+					mergedArray[i] = array1[firstArrayPointer];
+					firstArrayPointer++;
+				} else {
+					mergedArray[i] = array2[secondArrayPointer];
+					secondArrayPointer++;
+				}
+			}
+			i++;
+		}
 
-        return mergedArray;
-    }
+		return mergedArray;
+	}
 }
